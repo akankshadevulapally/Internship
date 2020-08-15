37dbe28b37f02eb0f6952b96b67bd7c4fec4d657
akankshadevulapally <akanksha2000devulapally@gmail.com>
2020-08-15 20:23:55 +0530

    hy

---
+++ Internship/Intern.js
@@ -0,0 +1,20 @@
+WebFontConfig = {
+				    google: { families: [ 'Roboto:400,700','Roboto Slab:400,500,700&amp;subset=latin,latin-ext','Roboto Condensed:400' ] }
+				  };
+				  (function() {
+				    var wf = document.createElement('script');
+				    wf.src = 'https://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+				    wf.type = 'text/javascript';
+				    wf.async = 'true';
+				    var s = document.getElementsByTagName('script')[0];
+				    s.parentNode.insertBefore(wf, s);
+				  })();
+				   var tp_postviews = {"url":"http:\/\/demo.themepacific.com\/bfastmag-pro\/wp-admin\/admin-ajax.php","nonce":"ac6afee2a4","pid":"2085"};
+				   var themepacific_wpreview = {"url":"http:\/\/demo.themepacific.com\/bfastmag-pro\/wp-admin\/admin-ajax.php","nonce":"634fc114e4"}; 
+				   var OwlCarouselRtl = {"rtl":""};</script> <script>var screenReaderText = {"expand":"<span class=\"screen-reader-text\">expand child menu<\/span>","collapse":"<span class=\"screen-reader-text\">collapse child menu<\/span>"};
+var stickyMenu = {"disable_sticky":"1"};
+ <script defer="" src="./BfastMag Pro – The Fastest News Magazine WP Theme_files/autoptimize_5e539e780cbded4f1707f10dd22bb77f.js.download"></script><script data-cfasync="false">!function(t){"use strict";t.loadCSS||(t.loadCSS=function(){});var e=loadCSS.relpreload={};if(e.support=function(){var e;try{e=t.document.createElement("link").relList.supports("preload")}
+ 	catch(t){e=!1}
+ 	return
+ 	 function(){return e}}(),e.bindMediaToggle=function(t){function e(){t.media=a}var a=t.media||"all";t.addEventListener?t.addEventListener("load",e):t.attachEvent&&t.attachEvent("onload",e),setTimeout(function(){t.rel="stylesheet",t.media="only x"}),setTimeout(e,3e3)},e.poly=function(){if(!e.support())for(var a=t.document.getElementsByTagName("link"),n=0;n<a.length;n++){var o=a[n];"preload"!==o.rel||"style"!==o.getAttribute("as")||o.getAttribute("data-loadcss")||(o.setAttribute("data-loadcss",!0),e.bindMediaToggle(o))}},!e.support()){e.poly();var a=t.setInterval(e.poly,500);t.addEventListener?t.addEventListener("load",function(){e.poly(),t.clearInterval(a)}):t.attachEvent&&t.attachEvent("onload",function(){e.poly(),t.clearInterval(a)})}"undefined"!=typeof exports?exports.loadCSS=loadCSS:t.loadCSS=loadCSS}("undefined"!=typeof global?global:this);
+</script>
